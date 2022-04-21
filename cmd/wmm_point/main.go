a7d35410aec7dcb21ce6277af4d63affb6be02c9
fastshout <131671231+fastshout@users.noreply.github.com>
2022-04-21 10:20:26 +0000

    7. Коммиты на решение задач.[#18069]

--- cmd/wmm_point/main.go
+++ cmd/wmm_point/main.go
@@ -1,2 +1,6 @@
 // wmm_point estimates the strength and direction of Earth's main Magnetic field for a given point/area.
 //
+// Usage is
+//  wmm_point --cof_file=WMM2020.COF --spherical [latitude] [longitude] [altitude] [date]
+//
+// The World Magnetic Model (WM
