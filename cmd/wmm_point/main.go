17737be8cb69e25317ae209f8fd9a83712751dee
fastshout <131671231+fastshout@users.noreply.github.com>
2022-04-21 19:37:00 +0000

    Ed

--- cmd/wmm_point/main.go
+++ cmd/wmm_point/main.go
@@ -3,4 +3,6 @@
 // Usage is
 //  wmm_point --cof_file=WMM2020.COF --spherical [latitude] [longitude] [altitude] [date]
 //
-// The World Magnetic Model (WM
+// The World Magnetic Model (WMM) for 2020
+// is a model of Earth's main Magnetic field.  The WMM
+// is recomputed every five (5) years, in years divis
