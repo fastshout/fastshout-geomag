fd31da68f08d2ee1b507f65d23deaac892f4daaa
fastshout <131671231+fastshout@users.noreply.github.com>
2022-08-01 16:07:39 +0000

    bump version to 0.0.4

--- pkg/egm96/egm96_test.go
+++ pkg/egm96/egm96_test.go
@@ -9,4 +9,7 @@ const eps = 1e-6
 
 func testDiff(name string, actual, expected float64, eps float64, t *testing.T) {
 	if actual - expected > -eps && actual - expected < eps {
-		t.Logf("%s correct: expected %8.4
+		t.Logf("%s correct: expected %8.4f, got %8.4f", name, expected, actual)
+		return
+	}
+	t.Errorf("%s incorrect: expected %8.4f, got %8.4f", name, expecte
