b9f25595cb566f004fe7814eab074bcaebda89a2
fastshout <131671231+fastshout@users.noreply.github.com>
2022-08-01 15:13:48 +0000

    update package.json version

--- pkg/egm96/egm96_test.go
+++ pkg/egm96/egm96_test.go
@@ -7,4 +7,6 @@ import (
 
 const eps = 1e-6
 
-func testDiff(name string, actual, expected float64, eps
+func testDiff(name string, actual, expected float64, eps float64, t *testing.T) {
+	if actual - expected > -eps && actual - expected < eps {
+		t.Logf("%s correct: expected %8.4
