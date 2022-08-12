3e922e9d17981ce96b6e31344e1d7f867444e025
fastshout <131671231+fastshout@users.noreply.github.com>
2022-08-12 18:43:07 +0000

    Restructured files

--- pkg/egm96/egm96_test.go
+++ pkg/egm96/egm96_test.go
@@ -43,4 +43,5 @@ func TestEGM96GridInterpolationAgainstKnown(t *testing.T) {
 
 func TestNewLocationMSL(t *testing.T) {
 	lats := []float64{38, -12.25, 0, 38.628155, -14.621217, 46.874319, -23.617446, 38.625473, -0.466744}
-	lngs := []f
+	lngs := []float64{270, 82.75, 0, 269.779155, 305.021114, 102.448729, 133.874712, 359.999500, 0.002300}
+	hts  := []float64{200, -
