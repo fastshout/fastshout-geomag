eb3abf3044ab92953434aa153f04c2fd27b21070
fastshout <131671231+fastshout@users.noreply.github.com>
2022-08-22 13:19:27 +0000

    USB: cdc-acm: fix pipe type of write endpoint

--- pkg/egm96/units.go
+++ pkg/egm96/units.go
@@ -1,3 +1,6 @@
 package egm96
 
-// Factors fo
+// Factors for converting between radians and degrees
+// and between meters and feet.
+const (
+	Deg = 1/57.29577951308232 // number of radians per degr
