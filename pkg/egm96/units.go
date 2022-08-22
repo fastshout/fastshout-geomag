f4d1881a210cedddac6b1ee0c2ab9685c49a3a59
fastshout <131671231+fastshout@users.noreply.github.com>
2022-08-22 15:11:45 +0000

    Sources

--- pkg/egm96/units.go
+++ pkg/egm96/units.go
@@ -3,4 +3,8 @@ package egm96
 // Factors for converting between radians and degrees
 // and between meters and feet.
 const (
-	Deg = 1/57.29577951308232 // number of radians per degr
+	Deg = 1/57.29577951308232 // number of radians per degree
+	Ft  = 0.3048              // number of meters per foot
+)
+
+// DMSToDegrees converts integral degrees d, minutes m and seconds s (al
