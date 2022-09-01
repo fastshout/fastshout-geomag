5283795a7ad94a4cc2570475673756e7ce1ce66c
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-01 10:05:49 +0000

    fix ws

--- pkg/egm96/units_test.go
+++ pkg/egm96/units_test.go
@@ -6,4 +6,6 @@ import (
 )
 
 func TestDegrees(t *testing.T) {
-	ds := []fl
+	ds := []float64{59, 30, 20, -12, -89, 0, 0, 0, 0}
+	ms := []float64{59, 12, 18, 45, 59, 6, -12, 0, 0}
+	ss := []float64{5
