4c2f88199418e5232663b915b1800f1afc1a0512
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-07 15:23:55 +0000

    edit .travis.yml

--- pkg/polynomial/legendre.go
+++ pkg/polynomial/legendre.go
@@ -9,4 +9,8 @@ var legendreFunctionCache = make(map[legendreFunctionIndex]Polynomial)
 
 // LegendrePolynomial returns a Polynomial object corresponding to
 // the Legendre Polynomial of degree n.
-// Once calculated initially, the polynomials are cached for faster future
+// Once calculated initially, the polynomials are cached for faster future access.
+func LegendrePolynomial(n int) (p Polynomial) {
+	p.c = make([]float64, n+1)
+	for m:=0; m<=n/2; m++ {
+		p.c[n-2*m]
