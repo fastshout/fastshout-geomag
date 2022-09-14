111ce6ab8a12d8447a1772359f14b575302570f6
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-14 11:15:06 +0000

    autogenerated files

--- pkg/polynomial/math.go
+++ pkg/polynomial/math.go
@@ -5,3 +5,9 @@ package polynomial
 // Handles up to n=20, beyond that it will overflow.
 func Factorial(n int) (z int) {
 	if n>1 {
+		return n*Factorial(n-1)
+	}
+	return 1
+}
+
+// FactorialRatio calculates the ratio of the factorial of the input int
