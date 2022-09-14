7e3b69d8f6ba4b0ea2ccf91ffab04bb23a694c45
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-14 08:45:30 +0000

    Update en.json

--- pkg/polynomial/math.go
+++ pkg/polynomial/math.go
@@ -1,4 +1,7 @@
 package polynomial
 
 // Factorial calculates the factorial of the input integer.
-// Doesn't handle negative numbers gracefull
+// Doesn't handle negative numbers gracefully, up to user to not pass them.
+// Handles up to n=20, beyond that it will overflow.
+func Factorial(n int) (z int) {
+	if n>1 {
