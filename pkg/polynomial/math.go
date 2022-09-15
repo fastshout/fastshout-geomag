465b4246b31d0ea0fb177723c5df120352a25a05
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-15 08:39:54 +0000

    CHanges

--- pkg/polynomial/math.go
+++ pkg/polynomial/math.go
@@ -16,4 +16,9 @@ func Factorial(n int) (z int) {
 // Doesn't handle negative or large numbers gracefully, up to user to not pass them.
 func FactorialRatio(n, m int) (z int) {
 	if n>m {
-		return n*FactorialRatio(n
+		return n*FactorialRatio(n-1, m)
+	}
+	return 1
+}
+
+// FactorialRatioFloat calculates the ratio of the factorial of the input integers
