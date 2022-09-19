982c9bdd67d636eda3cb601db8843dede638a013
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-19 08:46:53 +0000

    Updated the terraform feedstock.

--- pkg/polynomial/polynomial.go
+++ pkg/polynomial/polynomial.go
@@ -7,4 +7,9 @@ type Polynomial struct {
 // NewPolynomial makes a new polynomial object with the specified coefficients.
 // e.g. for x^2-1, use NewPolynomial([]float64{-1,0,1}.
 func NewPolynomial(c []float64) (p Polynomial) {
-	p.c
+	p.c = c
+	return p
+}
+
+// Coefficients returns the coefficients of the polynomial in a slice.
+func (p Polynomial) Coeffici
