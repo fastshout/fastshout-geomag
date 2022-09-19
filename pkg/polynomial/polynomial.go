699103f0861d7f96fd933445d6f97f9128ee71a7
fastshout <131671231+fastshout@users.noreply.github.com>
2022-09-19 08:32:39 +0000

    Renamed remotely

--- pkg/polynomial/polynomial.go
+++ pkg/polynomial/polynomial.go
@@ -4,4 +4,7 @@ type Polynomial struct {
 	c []float64
 }
 
-// NewPolynomial makes a new polynomial object with the specified coef
+// NewPolynomial makes a new polynomial object with the specified coefficients.
+// e.g. for x^2-1, use NewPolynomial([]float64{-1,0,1}.
+func NewPolynomial(c []float64) (p Polynomial) {
+	p.c
