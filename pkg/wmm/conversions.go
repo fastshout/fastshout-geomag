f614371dd3c526b292dec0a0c09a422f2e93ddaa
fastshout <131671231+fastshout@users.noreply.github.com>
2023-01-29 15:05:21 +0000

    Simplify tests

--- pkg/wmm/conversions.go
+++ pkg/wmm/conversions.go
@@ -6,4 +6,5 @@ import (
 
 type DecimalYear float64
 
-// DecimalYearsToTime converts an epoch-like fl
+// DecimalYearsToTime converts an epoch-like float64 year like 2015.0 to a Go time.Time.
+// Per document MIL-PRF-89500B Section 3.2, "Time is 
