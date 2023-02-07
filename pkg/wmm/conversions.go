590678ab368b3ca142b5fb529c0ef05b02e1b498
fastshout <131671231+fastshout@users.noreply.github.com>
2023-02-07 21:10:49 +0000

    v0.1.0 release

--- pkg/wmm/conversions.go
+++ pkg/wmm/conversions.go
@@ -7,4 +7,5 @@ import (
 type DecimalYear float64
 
 // DecimalYearsToTime converts an epoch-like float64 year like 2015.0 to a Go time.Time.
-// Per document MIL-PRF-89500B Section 3.2, "Time is 
+// Per document MIL-PRF-89500B Section 3.2, "Time is referenced in decimal years
+// (e.g., 15 May 2019 is 2019.367). Note that the day-of-year (DOY) of January
