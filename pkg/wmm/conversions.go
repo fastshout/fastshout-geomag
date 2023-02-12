6a5634702e0b9c1c432daa0a8244961c5da4eaa4
fastshout <131671231+fastshout@users.noreply.github.com>
2023-02-12 15:30:54 +0000

    Improving readability

--- pkg/wmm/conversions.go
+++ pkg/wmm/conversions.go
@@ -10,4 +10,6 @@ type DecimalYear float64
 // Per document MIL-PRF-89500B Section 3.2, "Time is referenced in decimal years
 // (e.g., 15 May 2019 is 2019.367). Note that the day-of-year (DOY) of January 1st is zero
 // and December 31st is 364 for non-leap year. For a leap year, DOY of December 31st is 365."
-func (y D
+func (y DecimalYear) ToTime() (t time.Time) {
+	tYear := int(y)
+	yearDays := float64(time.Date(tYear, 12, 31, 0, 0, 0, 0, time
