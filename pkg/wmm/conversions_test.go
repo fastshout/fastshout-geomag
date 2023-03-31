7f8cf6a1e1a3734d5183c546ce877c531e707394
fastshout <131671231+fastshout@users.noreply.github.com>
2023-03-31 19:41:15 +0000

    Header cleanup.

--- pkg/wmm/conversions_test.go
+++ pkg/wmm/conversions_test.go
@@ -7,4 +7,6 @@ import (
 )
 
 func TestDecimalYearsToTime(t *testing.T) {
-	ys := []D
+	ys := []DecimalYear{1995.0, 1996-1.0/365, 1997-1.0/366, 2004.0, 2017.5}
+	ts := []time.Time{
+		time.Date(1995, 1, 1, 0, 0, 0, 0, t
