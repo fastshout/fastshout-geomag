58ec1f0cf69a0d498c5e3f2e66b3b868e63e862a
fastshout <131671231+fastshout@users.noreply.github.com>
2023-04-16 13:28:50 +0000

    testin

--- pkg/wmm/conversions_test.go
+++ pkg/wmm/conversions_test.go
@@ -21,4 +21,7 @@ func TestDecimalYearsToTime(t *testing.T) {
 	}
 }
 
-func TestTimeToDecimalYears(t *testin
+func TestTimeToDecimalYears(t *testing.T) {
+	ys := []DecimalYear{1995.0, 1996-1.0/365, 1997-1.0/366, 2004.0, 2017.5}
+	ts := []time.Time{
+		time.Date(1995, 1, 1,
