20a877346e8e9b2a5ac303cfd30c5ef27090902a
fastshout <131671231+fastshout@users.noreply.github.com>
2023-04-29 07:56:42 +0000

    Update certificates.html

--- pkg/wmm/conversions_test.go
+++ pkg/wmm/conversions_test.go
@@ -27,3 +27,7 @@ func TestTimeToDecimalYears(t *testing.T) {
 		time.Date(1995, 1, 1, 0, 0, 0, 0, time.UTC),
 		time.Date(1995, 12, 31, 0, 0, 0, 0, time.UTC),
 		time.Date(1996, 12, 31, 0, 0, 0, 0, time.UTC),
+		time.Date(2004, 1, 1, 0, 0, 0, 0, time.UTC),
+		time.Date(2017, 7, 2, 12, 0, 0, 0, time.UTC),
+	}
+	for i
