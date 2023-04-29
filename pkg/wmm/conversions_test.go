07c695b0a4a868a040b27ec2c8f99ecfea66b8c8
fastshout <131671231+fastshout@users.noreply.github.com>
2023-04-29 19:31:30 +0000

    f2fs: don't need to call set_page_dirty for io error

--- pkg/wmm/conversions_test.go
+++ pkg/wmm/conversions_test.go
@@ -30,4 +30,6 @@ func TestTimeToDecimalYears(t *testing.T) {
 		time.Date(2004, 1, 1, 0, 0, 0, 0, time.UTC),
 		time.Date(2017, 7, 2, 12, 0, 0, 0, time.UTC),
 	}
-	for i
+	for i, tt := range ts {
+		d := TimeToDecimalYears(tt)
+		testDiff(fmt.Sprintf("%v to decimal year", tt), floa
