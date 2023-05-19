648df09be661d19de597f8efeaee9e2061e756ab
fastshout <131671231+fastshout@users.noreply.github.com>
2023-05-19 12:38:54 +0000

    more mods

--- pkg/wmm/conversions_test.go
+++ pkg/wmm/conversions_test.go
@@ -40,4 +40,6 @@ func TestTimeToDecimalYearRoundTrips(t *testing.T) {
 	ys := []DecimalYear{1995.0, 1996-1.0/365, 1997-1.0/366, 2004.0, 2017.5}
 	ts := []time.Time{
 		time.Date(1995, 1, 1, 0, 0, 0, 0, time.UTC),
-		time.Date(1995, 1
+		time.Date(1995, 12, 31, 0, 0, 0, 0, time.UTC),
+		time.Date(1996, 12, 31, 0, 0, 0, 0, time.UTC),
+		time.Date(2004, 1, 1, 0, 0, 0, 0,
